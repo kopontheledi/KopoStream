166d6cf59b237770bedf21efea00fcf4c4db2751
Kopo Ntheledi <kopontheledi@gmail.com>
2023-08-17 14:56:50 +0200

    updates

--- stream/src/main.jsx
+++ stream/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import './Styles/index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <App className="app" />
+  </BrowserRouter>,
   </React.StrictMode>,
+ 
 )
