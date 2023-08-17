ff9e95f2ad3c52b9b6d1ad0cdfc01b65b628c17a
Kopo Ntheledi <kopontheledi@gmail.com>
2023-08-17 15:05:19 +0200

    supabase

--- stream/src/main.jsx
+++ stream/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
+
 import './Styles/index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-  <BrowserRouter>
+ 
     <App className="app" />
-  </BrowserRouter>,
+  
   </React.StrictMode>,
  
 )
